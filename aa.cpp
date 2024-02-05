e9638f3db0fe6974da98c7431aa8ea1455cef746
Lucas Costa <122161095+Lucasbc47@users.noreply.github.com>
2024-02-05 20:42:59 -0300

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -1,30 +1,42 @@
 // aa.cpp
 #include "aa.h"
 
-PlayerObj::PlayerObj(int x, int y, int speed) : x(x), y(y), speed(speed) {
+PlayerObj::PlayerObj(int x, int y, int speed) : x(x), y(y), speed(speed)
+{
     playerTexture = LoadTexture("img/spaceship.png");
 }
 
-PlayerObj::~PlayerObj() {
+PlayerObj::~PlayerObj()
+{
     UnloadTexture(playerTexture);
 }
 
-void PlayerObj::Draw() {
-    DrawTexture(playerTexture, x, y, WHITE);
+void PlayerObj::Draw()
+{
+    // dá um resize pra 64x64
+    Rectangle sourceRec = {0.0f, 0.0f, (float)playerTexture.width, (float)playerTexture.height};
+    Rectangle destRec = {(float)x, (float)y, 64, 64};
+
+    DrawTexturePro(playerTexture, sourceRec, destRec, Vector2{0, 0}, 0, WHITE);
 }
 
-void PlayerObj::Movement() {
-    if (IsKeyDown(KEY_A)) {
+void PlayerObj::Movement()
+{
+    if (IsKeyDown(KEY_A))
+    {
         x = x - speed;
     }
 
-    if (IsKeyDown(KEY_D)) {
+    if (IsKeyDown(KEY_D))
+    {
         x = x + speed;
     }
-    if (IsKeyDown(KEY_W)) {
+    if (IsKeyDown(KEY_W))
+    {
         y = y - speed;
     }
-    if (IsKeyDown(KEY_S)) {
+    if (IsKeyDown(KEY_S))
+    {
         y = y + speed;
     }
 }
