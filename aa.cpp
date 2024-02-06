3a57ffa63122558949107fad5ead2a47540f6226
Skillter <84723394+Skillterer@users.noreply.github.com>
2024-02-05 21:52:01 -0300

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -1,7 +1,9 @@
 // aa.cpp
 #include "aa.h"
+#include <iostream>
+#include <algorithm>
 
-PlayerObj::PlayerObj(int x, int y, int speed) : x(x), y(y), speed(speed)
+PlayerObj::PlayerObj(int x, int y, int speed, float objRotation) : x(x), y(y), speed(speed), objRotation(objRotation)
 {
     playerTexture = LoadTexture("img/spaceship.png");
 }
@@ -13,24 +15,34 @@ PlayerObj::~PlayerObj()
 
 void PlayerObj::Draw()
 {
-    // dá um resize pra 64x64
-    Rectangle sourceRec = {0.0f, 0.0f, (float)playerTexture.width, (float)playerTexture.height};
-    Rectangle destRec = {(float)x, (float)y, 64, 64};
+    // Create source and destination rectangles
+    Rectangle sourceRec = { 0.0f, 0.0f, static_cast<float>(playerTexture.width), static_cast<float>(playerTexture.height) };
+    Rectangle destRec = { static_cast<float>(x), static_cast<float>(y), GetScreenWidth()*0.1f, GetScreenWidth()*0.1f };
 
-    DrawTexturePro(playerTexture, sourceRec, destRec, Vector2{0, 0}, 0, WHITE);
+    // Draw the resized texture
+    DrawTexturePro(playerTexture, sourceRec, destRec, Vector2{ GetScreenWidth()*0.05f, GetScreenWidth()*0.05f }, objRotation, WHITE);
 }
+
 
 void PlayerObj::Movement()
 {
+    const float rotationSpeed = 2.5f;
     if (IsKeyDown(KEY_A))
     {
         x = x - speed;
-    }
-
-    if (IsKeyDown(KEY_D))
+        objRotation = std::max(objRotation - rotationSpeed, -15.0f);
+    } else if (IsKeyDown(KEY_D))
     {
         x = x + speed;
+        objRotation = std::min(objRotation + rotationSpeed, 15.0f);
+    } else {
+        // Se nenhuma tecla de rotação está pressionada, suavize a rotação de volta para 0
+        if (objRotation > 0.0f)
+            objRotation = std::max(objRotation - rotationSpeed, 0.0f);
+        else if (objRotation < 0.0f)
+            objRotation = std::min(objRotation + rotationSpeed, 0.0f);
     }
+
     if (IsKeyDown(KEY_W))
     {
         y = y - speed;
@@ -39,4 +51,5 @@ void PlayerObj::Movement()
     {
         y = y + speed;
     }
+
 }
