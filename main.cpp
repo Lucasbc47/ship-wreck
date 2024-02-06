7a30776c1b3ab88c978fc0a76243ebb432860d31
Skillter <84723394+Skillterer@users.noreply.github.com>
2024-02-06 19:28:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "raylib.h"
 #include "spaceship.hpp"
 #include "asteroids.hpp"
+#include "hpdraw.hpp"
 
 #define HEIGHT 800
 #define WIDTH 600
 #define TITLE "Ship Wreck"
-     
+    
 
 int main(void)
 {
@@ -14,6 +15,7 @@ int main(void)
     SetTargetFPS(60);
 
     Ship ship(400, 400, 2, 0);
+    Hp hp(50);
     InitAsteroids();
 
     while (!WindowShouldClose())
@@ -25,6 +27,7 @@ int main(void)
         UpdateAsteroids();
         DrawAsteroids();
 
+        hp.Draw();
         ship.Movement();
         ship.Draw();
 
